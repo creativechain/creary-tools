3d51793c051427bc0689946b5296925ad06d4b69
ander7agar <ander7agar@gmail.com>
2019-01-04 07:16:38 +0100

    init commit

---
+++ bin/creary.js
@@ -0,0 +1,184 @@
+#! /usr/bin/env node
+
+let crea = require('@creativechain-fdn/crea-js');
+let program = require('commander');
+let mysql = require('mysql');
+
+function createAuth(key) {
+    return {
+        weight_threshold: 1,
+        account_auths: [],
+        key_auths: [
+            [key, 1]
+        ]
+    }
+}
+
+function setOptions() {
+    let apiOptions = {
+        url: 'https://crea.owldevelopers.site',
+        addressPrefix: 'CREA',
+        chainId: '0000000000000000000000000000000000000000000000000000000000000000',
+    };
+
+    crea.api.setOptions(apiOptions);
+
+    crea.config.set('chain_id', apiOptions.chainId);
+    crea.config.set('address_prefix', apiOptions.addressPrefix);
+}
+
+setOptions();
+
+program
+    .version('0.0.1')
+    .description('Creary Blockchain Tools');
+
+//ACCOUNT CREATION COMMAND
+program.command('new-account <creator> <wif> <user> <active> <posting> <memo> <owner> <json> <fee> <cgy>')
+    .description('Create new Blockchain account')
+    .action(function (creator, wif, user, active, posting, memo, owner, json, fee, cgy) {
+
+        //console.log(creator, wif, user, active, posting, memo, owner, json, fee);
+        crea.broadcast.accountCreate(wif, fee, creator, user, createAuth(owner),
+            createAuth(active), createAuth(posting), memo, json, function (err, result) {
+
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(result);
+
+                    crea.broadcast.transferToVesting(wif, creator, user, cgy, function (err, result) {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log(result);
+                        }
+                    })
+                }
+            })
+    });
+
+//SEARCHER COMMAND
+program.command('store-blocks <host> <user> <password> <database> <block>')
+    .description('Store Creary blockchain blocks in a MySQL Database')
+    .action(function (host, user, password, database, block) {
+
+        block = parseInt(block);
+        let fn = async function () {
+            let mysqlConnection = mysql.createConnection({host, user, password, database});
+
+            mysqlConnection.connect();
+
+            while (true) {
+
+                try {
+                    let p = await crea.api.getBlockAsync(block);
+
+                    let onDbConnected = function () {
+                        if (p) {
+
+                            let txs = p.transactions;
+                            let timestamp = new Date(p.timestamp);
+
+                            console.log('Block', block, 'with', txs.length, ' operations' );
+                            txs.forEach(function (tx) {
+                                let operations = tx.operations;
+
+                                operations.forEach(function (op) {
+                                    let opType = op[0];
+                                    let opData = op[1];
+
+                                    console.log('Op:', opType);
+                                    if (opType === 'comment') {
+
+                                        let permlink = opData.permlink;
+                                        let author = opData.author;
+                                        console.log('Processing', author, permlink);
+                                        if (!opData.parent_author) {
+
+                                            try {
+                                                opData.metadata = JSON.parse(opData.json_metadata);
+                                            } catch (e) {
+                                                console.error('Failed parsing metadata');
+                                            }
+
+                                            mysqlConnection.query('SELECT * FROM crea_content WHERE author = ? AND permlink = ?', [author, permlink], function (error, results, fields) {
+                                                if (error) {
+                                                    console.log(error);
+                                                } else if (results) {
+                                                    let query;
+                                                    let params = [];
+                                                    if (results.length) {
+                                                        //Modify
+                                                        console.log('Updating registry', author, permlink);
+                                                        query = 'UPDATE crea_content SET title = ?, license = ?, adult = ?, description = ?, tags = ? WHERE author = ? AND permlink = ?;';
+                                                    } else {
+                                                        query = 'INSERT INTO crea_content (author, permlink, title, license, adult, description, tags, creation_date, hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+                                                        params.push(author);
+                                                        params.push(permlink);
+                                                    }
+
+                                                    params.push(opData.title);
+
+                                                    if (opData.metadata) {
+                                                        params.push(opData.metadata.license);
+                                                        params.push(opData.metadata.adult ? 1: 0);
+                                                        params.push(opData.metadata.description);
+
+                                                        try {
+                                                            params.push(JSON.stringify(opData.metadata.tags));
+                                                        } catch (e) {
+                                                            console.log('Fail encoding tags');
+                                                            params.push('[]')
+                                                        }
+                                                    } else {
+                                                        params.push(-1);
+                                                        params.push(0);
+                                                        params.push('');
+                                                        params.push('');
+                                                    }
+
+                                                    params.push(timestamp.getTime());
+                                                    params.push('');
+
+                                                    mysqlConnection.query(query, params, function (err, results, fields) {
+                                                        if (err) {
+                                                            console.error(err);
+                                                        }
+                                                    });
+                                                }
+                                            })
+                                        }
+                                    }
+                                });
+                            });
+
+                            block++;
+                        }
+                    };
+
+                    mysqlConnection.ping(function (err) {
+                        if (err) {
+                            mysqlConnection.destroy();
+                            mysqlConnection = mysql.createConnection({host, user, password, database});
+                            mysqlConnection.connect();
+                            onDbConnected();
+                        } else {
+                            onDbConnected();
+                        }
+                    });
+
+
+                } catch (e) {
+
+                }
+            }
+        };
+
+        fn();
+
+    });
+
+program.parse(process.argv);
+
+
